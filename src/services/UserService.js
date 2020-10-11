429ee43d90ef4135ea26640bc1205375669a9d55
ilijevskia <ilijevskia@ae.com>
2020-10-11 17:23:26 +0200

    Change Users rest api url

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL = "http://localhost:8080/api/user";
+const USERS_REST_API_URL = "http://aws-haproxy.ddns.net:30010/api/user";
 
 class UserService {
 
